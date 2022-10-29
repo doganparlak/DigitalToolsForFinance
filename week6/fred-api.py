a48ac0205a5c1b9de0dbf10f4375960a82361968
Lelamil <70172102+Lelamil@users.noreply.github.com>
2022-10-29 14:29:48 +0200

    Add files via upload

--- week6/fred-api.py
+++ week6/fred-api.py
@@ -6,7 +6,7 @@ from fredapi import Fred
 
 # Source https://lvngd.com/blog/fred-api-python/
 individual_fred_key = "abc123" # fake api key
-#individual_fred_key = os.environ['fredapiKey'] # valid api key
+#individual_fred_key = os.environ['fredApiKey'] # valid api key
 
 # Question 2
 ROOT_URL = "https://api.stlouisfed.org"
@@ -22,22 +22,22 @@ params = {
     'observation_start': '2020-01-01'
 }
 
-# Send request to the Fred server & receive a response
+# Send a request to the Fred server & receive a response
 resp = requests.get(f"{ROOT_URL}{ENDPOINT}", params = params)
 
-# resp == 400 (failed) with fake api key, but resp == 200 with valid key
+# resp == 400 (failed) with the fake api key, but resp == 200 with a valid key
 print(resp) 
 
-# Alternatively: use wrapper by mortdata
+# Alternative solution: use wrapper by mortdata
 # Source https://github.com/mortada/fredapi, https://pypi.org/project/fredapi/
 fred = Fred(api_key = individual_fred_key)
 unRateSeries = fred.get_series('UNRATE', '2020-01-01') # returns pandas Series
 
-# timeseries plot
+# Create a timeseries plot
 unRateSeries.plot()
 pyplot.show()
 
-# density plot
+# Density plot
 unRateSeries.plot(kind='kde')
 pyplot.show()
 
